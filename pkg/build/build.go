654b54ecdac2594486ab9668ce52685cdbf03c8d
Rugwiro Mbdanda Valentin <rugwirobaker@gmail.com>
2019-08-07 11:03:19 +0200

    Add build package

---
+++ pkg/build/build.go
@@ -0,0 +1,30 @@
+package build
+
+import "fmt"
+
+// Details represents known data for a given build
+type Details struct {
+	Version string `json:"version,omitempty"`
+	Date    string `json:"date,omitempty"`
+}
+
+var version, buildDate string
+
+// String returns build details as a string with formatting
+// suitable for console output.
+//
+// i.e.
+// Build Details:
+//  Version:        v0.1.0-155-g1a20f8b
+//  Date:           2018-11-05-14:33:14-UTC
+func String() string {
+	return fmt.Sprintf("Build Details:\n  Version:\t%s\n  Date:\t\t%s", version, buildDate)
+}
+
+// Data returns build details as a struct
+func Data() Details {
+	return Details{
+		Version: version,
+		Date:    buildDate,
+	}
+}
